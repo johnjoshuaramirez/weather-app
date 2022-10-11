235b46e9eb8e549b2f2cd94cda185109f6f0dc66
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-10-11 03:47:46 -0800

    Finishing weather api

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ locationBtn.addEventListener("click", () => {
 function onSuccess(position) {
    const lat = position.coords.latitude;
    const lon = position.coords.longitude;
-   const api = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit={limit}&appid=e4ee73f3b3b62a5cbfafd70ca95801ca`
+   api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=e4ee73f3b3b62a5cbfafd70ca95801ca`;
+   loadData();
 }
 
 function onError(error) {
@@ -34,7 +35,8 @@ function onError(error) {
 }
 
 async function requestAPI(city) {
-   const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e4ee73f3b3b62a5cbfafd70ca95801ca`;
+   api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=e4ee73f3b3b62a5cbfafd70ca95801ca`;
+   loadData();
 }
 
 async function loadData() {
@@ -47,7 +49,39 @@ async function loadData() {
 }
 
 function weatherDetails(info) {
-   console.log(info);
+   infoTxt.classList.replace("pending", "error");
+   if (info.cod === "404") {
+      infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+   } else {
+      const city = info.name;
+      const country = info.sys.country;
+      const { description, id} = info.weather[0];
+      const { feels_like, humidity, temp } = info.main;
+
+      if (id == 800) {
+         wIcon.src = "icons/clear.svg";
+      } else if (id >= 200 && id <= 232) {
+         wIcon.src = "icons/storm.svg";
+      } else if (id >= 600 && id <= 622) {
+         wIcon.src = "icons/snow.svg";
+      } else if (id >= 701 && id <= 781) {
+         wIcon.src = "icons/haze.svg";
+      } else if (id >= 801 && id <= 804) {
+         wIcon.src = "icons/cloud.svg";
+      } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+         wIcon.src = "icons/rain.svg";
+      }
+
+      wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+      wrapper.querySelector(".weather").innerText = description;
+      wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+      wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+      wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+      
+      infoTxt.classList.remove("pending", "error");
+      wrapper.classList.add("active");
+      console.log(info);
+   }
 }
 
 
