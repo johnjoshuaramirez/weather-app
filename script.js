ccbe18aa5d6b632a8ee570b6a86d228d9580fd4c
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2022-10-11 03:59:58 -0800

    Finished

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const wrapper = document.querySelector(".wrapper"),
-arrowBack = wrapper.querySelector(".header i"),
 inputPart = wrapper.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
 inputField = inputPart.querySelector("input"),
 locationBtn = inputPart.querySelector("button"),
-weatherPart = wrapper.querySelector(".weather-part"),weatherIcon = weatherPart.querySelector("img");
+weatherPart = wrapper.querySelector(".weather-part"),wIcon = weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
 
 let api;
 
@@ -83,5 +83,9 @@ function weatherDetails(info) {
       console.log(info);
    }
 }
+
+arrowBack.addEventListener("click", () => {
+   wrapper.classList.remove("active");
+});
 
 
