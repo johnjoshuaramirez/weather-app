1e6638e8c66d9dc1563bf7aa10a89999988babb8
John Joshua Ramirez <johnjoshuaramirez.official@gmail.com>
2023-03-28 21:18:50 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+const input = document.querySelector('.input');
+const search_button = document.querySelector('.search-button');
+const loc = document.querySelector('.location');
+const image = document.querySelector('.image');
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temperature');
+const information = document.querySelector('.information');
+
+async function getData() { 
+   try {
+      const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=ca895c8dee0e47768df11549232703&q=${input.value}`, { mode: 'cors' });
+      const data = await response.json();
+      image.src = data.current.condition.icon
+      weather.innerText = data.current.condition.text;
+      temperature.innerText = `${data.current.temp_c} °C`;
+      loc.innerText = `${data.location.name}, ${data.location.country}`;
+      information.classList.remove('hidden');
+   } catch (err) {
+      console.error('Error', err);
+   }
+}
+
+search_button.addEventListener('click', getData);
